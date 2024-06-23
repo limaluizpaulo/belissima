fb5b02807c3634059fc90d77e4d39f05ee0b4eca
Luiz <limaluizpaulo@live.com>
2024-06-23 00:27:28 -0300

    feat: Improve styling and responsiveness of various components

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -5,9 +5,13 @@ import './style.css'
 const Hero: React.FC = () => {
     return (
         <section id="inicio" className="hero">
-            <img src="/header.jpeg" alt="Produtos" className="hero-image" />
+            <img
+                src={window.innerWidth < 768 ? "/sobremim.png" : "/header.jpeg"}
+                alt="Produtos"
+                className="hero-image"
+            />
             <h1 className="hero-title">A Potência do Pará <br /> na Beleza Capilar</h1>
-            
+
             <img src="/cabelo.jpg" alt="" className="bg-image" />
         </section>
     );
