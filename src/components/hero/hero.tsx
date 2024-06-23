35a8e962f46511d6ce4a7b78855cdf7e253e77a7
Luiz <limaluizpaulo@live.com>
2024-06-23 00:40:23 -0300

    feat: Add lazy loading for hero component images

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -1,12 +1,33 @@
-'use client';
-import './style.css'
+'use client'
+import React, { useState, useEffect } from 'react';
+import './style.css';
+
 
 
 const Hero: React.FC = () => {
+    const [imageSrc, setImageSrc] = useState("/header.jpeg");
+
+    useEffect(() => {
+        // Esta função só será executada no lado do cliente, após a montagem do componente
+        const handleResize = () => {
+            const src = window.innerWidth < 768 ? "/sobremim.png" : "/header.jpeg";
+            setImageSrc(src);
+        };
+
+        // Definir a imagem correta ao carregar
+        handleResize();
+
+        // Adicionar listener para mudanças de tamanho da janela
+        window.addEventListener('resize', handleResize);
+
+        // Limpar o listener ao desmontar o componente
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
     return (
         <section id="inicio" className="hero">
             <img
-                src={window.innerWidth < 768 ? "/sobremim.png" : "/header.jpeg"}
+                src={imageSrc}
                 alt="Produtos"
                 className="hero-image"
             />
