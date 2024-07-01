7a969baaf4b48643016ac8fd610e694970afe8f6
Luiz <limaluizpaulo@live.com>
2024-07-01 05:10:37 -0300

    refactor: Update product images and descriptions

--- src/components/produtos/Produtos.tsx
+++ src/components/produtos/Produtos.tsx
@@ -8,13 +8,16 @@ const Produtos: React.FC = () => {
   <div className='produtos'>
   
   <div className='produto'>
-  <img src='/50.png' alt='Shampoo Garden Hair' />
+  <img src='/50.png' alt='Excellence tree ' />
+  <p className="descricao"  >Excellence tree  </p>
   </div>
   < div className='produto'>
-  <img src='/24.png' alt='Condicionador Garden Hair' />
+  <img src='/PO.png' alt='Pó descolorante' />
+  <p className="descricao"  >Pó descolorante </p>
   </div>
   < div className='produto'>
-  <img src='/PO.png' alt='Condicionador Garden Hair' />
+  <img src='/24.png' alt='Eko line' />
+  <p className="descricao"  >Eko line</p>
   </div>
 </div>
 </div>
